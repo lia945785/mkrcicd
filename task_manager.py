ec1ccd7c328bb2b2b23c3d5a5a2c421b80876a0c
anna <lia945785l@gmail.com>
2026-04-08 15:48:12 +0300

    feat: implement delete_task method

--- task_manager.py
+++ task_manager.py
@@ -48,7 +48,8 @@ class TaskManager:
         self.save_tasks()
 
     def delete_task(self, task_id):
-        pass
+        self.tasks = [task for task in self.tasks if task.id != task_id]
+        self.save_tasks()
 
     def list_tasks(self, sort_by="priority"):
         pass
